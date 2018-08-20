8014dd72819ac6efeb270869988b91531df19a4a
kazakhgeometry <41499708+kazakhgeometry@users.noreply.github.com>
2018-08-20 17:47:47 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,71 +1,47 @@
-var cy = window.cy = cytoscape({
+$(function(){ // on dom ready
+
+var cy = cytoscape({
+
   container: document.getElementById('cy'),
 
   boxSelectionEnabled: false,
   autounselectify: true,
-
-  layout: {
-    name: 'dagre'
-  },
-
-  style: [
-    {
-      selector: 'node',
-      style: {
-        'content': 'data(id)',
-        'text-opacity': 0.5,
-        'text-valign': 'center',
-        'text-halign': 'right',
-        'background-color': '#11479e'
-      }
-    },
-
-    {
-      selector: 'edge',
-      style: {
-        'curve-style': 'bezier',
-        'width': 4,
-        'target-arrow-shape': 'triangle',
-        'line-color': '#9dbaea',
-        'target-arrow-color': '#9dbaea'
-      }
-    }
-  ],
+  maxZoom: 2,
+  minZoom: 0.5,
 
   elements: {
     nodes: [
-      { data: { id: 'n0' } },
-      { data: { id: 'n1' } },
-      { data: { id: 'n2' } },
-      { data: { id: 'n3' } },
-      { data: { id: 'n4' } },
-      { data: { id: 'n5' } },
-      { data: { id: 'n6' } },
-      { data: { id: 'n7' } },
-      { data: { id: 'n8' } },
-      { data: { id: 'n9' } },
-      { data: { id: 'n10' } },
-      { data: { id: 'n11' } },
-      { data: { id: 'n12' } },
-      { data: { id: 'n13' } },
-      { data: { id: 'n14' } },
-      { data: { id: 'n15' } },
-      { data: { id: 'n16' } }
-    ],
-    edges: [
-      { data: { source: 'n0', target: 'n1' } },
-      { data: { source: 'n1', target: 'n2' } },
-      { data: { source: 'n1', target: 'n3' } },
-      { data: { source: 'n4', target: 'n5' } },
-      { data: { source: 'n4', target: 'n6' } },
-      { data: { source: 'n6', target: 'n7' } },
-      { data: { source: 'n6', target: 'n8' } },
-      { data: { source: 'n8', target: 'n9' } },
-      { data: { source: 'n8', target: 'n10' } },
-      { data: { source: 'n11', target: 'n12' } },
-      { data: { source: 'n12', target: 'n13' } },
-      { data: { source: 'n13', target: 'n14' } },
-      { data: { source: 'n13', target: 'n15' } },
+      { data: { id: 'n', label: 'Tap me' } }
     ]
   },
+
+  layout: {
+    name: 'grid',
+    padding: 100
+  },
+
+  ready: function(){
+    window.cy = this;
+  },
+
+  style: 'node { content: data(label); }'
 });
+
+// you can use qtip's regular options
+// see http://qtip2.com/
+cy.$('#n').qtip({
+  content: 'Hello!',
+  position: {
+    my: 'top center',
+    at: 'bottom center'
+  },
+  style: {
+    classes: 'qtip-bootstrap',
+    tip: {
+      width: 16,
+      height: 8
+    }
+  }
+});
+
+}); // on dom ready
